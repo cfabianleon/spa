f62f6228808ec36f80885885dda254babd5d043b
Fabian <Fabians-MacBook-Pro@Macbooks-MacBook-Pro.local>
2018-06-14 21:10:42 -0500

    prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
+import { APP_ROUTING } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// rutas 
+import {APP_ROUTING} from './app.routes';
+
+// componentes
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HomeComponent, NavbarComponent, HeroesComponent],
+  imports: [ BrowserModule, APP_ROUTING],
   providers: [],
   bootstrap: [AppComponent]
 })
